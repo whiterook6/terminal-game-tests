6304f7786b74b55fd2cbe85b3357713abd5596be
Tim Graboski <whiterook6@gmail.com>
2024-04-24 23:09:34 -0700

    primitive tunneler

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
+import { Terrain } from "./terrain/Terrain";
+import { Direction, Tunneler } from "./terrain/Tunneler";
+
 const run = () =>{
-  
+  const terrain = new Terrain(50, 20, "█");
+  const tunneler = new Tunneler(terrain, 2, 100, 0, 10, Direction.EAST);
+  while (!tunneler.isDead()) {
+    console.log("Digging...");
+    tunneler.dig();
+  }
+  console.log(terrain.getMap().join("\n"));
 }
 
 run();
