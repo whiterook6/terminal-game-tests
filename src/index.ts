7c1259c84a9b64c8d48a04f6f74062cce72174da
Tim Graboski <whiterook6@gmail.com>
2024-05-05 22:48:20 -0700

    trying out windows with new framebuffer

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,94 @@
 import ansi from "ansi";
-import { Framebuffer } from "./framebuffer/Framebuffer";
-import { ViewXY } from "./types";
+import {Framebuffer} from "./framebuffer/Framebuffer"
+import { TitlePosition, Window } from "./ui/Window";
 
-const run = () => {
-  const cursor = ansi(process.stdout).hide();
-  const buffer = Framebuffer({
-    viewWidth: process.stdout.columns,
-    viewHeight: process.stdout.rows
+const run = () =>{
+  const cursor = ansi(process.stdout);
+
+  cursor.hide().brightWhite();
+
+  const framebuffer = Framebuffer({
+    viewHeight: process.stdout.rows,
+    viewWidth: process.stdout.columns
   });
 
-  process.stdout.addListener("resize", () => {
-    buffer.resize({
-      viewWidth: process.stdout.columns,
-      viewHeight: process.stdout.rows
+  let logWindow: string[];
+  let playerWindow: string[];
+  let mapWindow: string[];
+  let inventoryWindow: string[];
+
+  const buildWindows = () => {
+    const screenWidth = process.stdout.columns;
+    const screenHeight = process.stdout.rows;
+
+    logWindow = Window({
+      width: Math.floor(screenWidth / 2),
+      height: 7,
+      title: "Log",
+      titlePosition: TitlePosition.BOTTOM_RIGHT
     });
+    playerWindow = Window({
+      width: Math.ceil(screenWidth / 2),
+      height: 7,
+      title: "Player",
+      titlePosition: TitlePosition.BOTTOM_LEFT
+    });
+    mapWindow = Window({
+      width: screenWidth - 25,
+      height: screenHeight - playerWindow.length + 1,
+      title: "Map",
+      titlePosition: TitlePosition.TOP
+    });
+    inventoryWindow = Window({
+      width: 25,
+      height: screenHeight - playerWindow.length + 1,
+      title: "Inventory",
+      titlePosition: TitlePosition.TOP_LEFT
+    });
+  };
+
+  buildWindows();
+  process.stdout.addListener("resize", () => {
+    framebuffer.resize({
+      viewHeight: process.stdout.rows,
+      viewWidth: process.stdout.columns
+    });
+    buildWindows();
   });
 
-  const textPosition: ViewXY = {
-    viewX: 0,
-    viewY: 0
-  };
-  let speedX = 50 / 60;
-  let speedY = 15 / 60;
-  const update = () => {
-    buffer.clear();
-    textPosition.viewX += speedX;
-    textPosition.viewY += speedY;
-    if (textPosition.viewX >= buffer.width()){
-      speedX = -50 / 60;
-    } else if (textPosition.viewX < -5) {
-      speedX = 50 / 60;
+  const render = () => {
+    framebuffer.clear();
+    for (let y = 0; y < logWindow.length; y++) {
+      framebuffer.write({
+        viewX: 0,
+        viewY: y,
+      }, [[logWindow[y], [255, 255, 255], [0, 0, 0]]]);
     }
-    if (textPosition.viewY >= buffer.height()){
-      speedY = -15 / 60;
-    } else if (textPosition.viewY < 0) {
-      speedY = 15 / 60;
+    for (let y = 0; y < playerWindow.length; y++) {
+      framebuffer.write({
+        viewX: logWindow[0].length,
+        viewY: y,
+      }, [[playerWindow[y], [255, 255, 255], [0, 0, 0]]]);
     }
-    
-    const xString = textPosition.viewX.toFixed(2);
-    const yString = textPosition.viewY.toFixed(2);
-    buffer.write(textPosition, [
-      ["[", [255, 255, 255], [0, 0, 0]],
-      [xString, [255, 0, 0], [50, 50, 50]],
-      [",", [0, 255, 0], [0, 0, 0]],
-      [yString, [0, 0, 255], [50, 50, 50]],
-      ["]", [255, 255, 255], [0, 0, 0]]
-    ]);
-    
-    buffer.render(cursor);
-  };
-  let interval = setInterval(update, 1000 / 60);
+    for (let y = 0; y < mapWindow.length; y++) {
+      framebuffer.write({
+        viewX: 0,
+        viewY: y + logWindow.length,
+      }, [[mapWindow[y], [255, 255, 255], [0, 0, 0]]]);
+    }
+    for (let y = 0; y < inventoryWindow.length; y++) {
+      framebuffer.write({
+        viewX: mapWindow[0].length,
+        viewY: y + logWindow.length,
+      }, [[inventoryWindow[y], [255, 255, 255], [0, 0, 0]]]);
+    }
+
+    framebuffer.render(cursor);
+  }
+
+  const interval = setInterval(() => {
+    render();
+  }, 1000 / 60);
 
   process.on('SIGINT', function() {
     cursor.goto(0, 0).show().reset().bg.reset().eraseLine();
