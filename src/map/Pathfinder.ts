543059a81231739d8f8ba9f6bfd9cc28e0b12e8f
Tim Graboski <tgraboski@tutela.com>
2024-06-27 12:43:16 -0700

    Pathfinder takes a cost function as well as an is-walkable function

--- src/map/Pathfinder.ts
+++ src/map/Pathfinder.ts
@@ -13,7 +13,15 @@ export type Path = Cell[];
 /**
  * A function to check whether a given cell is walkable or not.
  */
-export type IsWalkable = (from: Cell, to: Cell) => boolean;
+export type IsWalkableFN = (from: Cell, to: Cell) => boolean;
+
+/**
+ * A function to calculate the cost of moving from one cell to another.
+ */
+export type CostFN = (from: Cell, to: Cell) => number;
+export const manhattanCost: CostFN = (from: Cell, to: Cell) => Math.abs(from[0] - to[0]) + Math.abs(from[1] - to[1]);
+export const euclideanCost: CostFN = (from: Cell, to: Cell) => Math.sqrt(Math.pow(from[0] - to[0], 2) + Math.pow(from[1] - to[1], 2));
+
 
 type FrontierNode = [
   number, // x
@@ -25,17 +33,22 @@ type FrontierNode = [
 ];
 
 export class Pathfinder {
-  public findPath = (start: Cell, goal: Cell, isWalkable: IsWalkable): Path => {
+  public findPath = (start: Cell, goal: Cell, isWalkable: IsWalkableFN, costFN: CostFN): Path => {
     const frontier = new Heap<FrontierNode>((a, b) => a[4] + a[5] - b[4] - b[5]);
-    frontier.push(this.makeNode(start, start, 0, this.getCost(start, goal)));
+    frontier.push(this.makeNode(start, start, 0, costFN(start, goal)));
     const visited: FrontierNode[] = [];
 
+    // loop until we find the goal or run out of iterations
     const iterationLimit = 10_000;
     let iterations = 0;
     while (!frontier.empty() && iterations < iterationLimit){
       iterations++;
+
+      // get the current node
       const current = frontier.pop();
       if (!current){
+        break;
+      } else if (current[0] === goal[0] && current[1] === goal[1]){
         break;
       }
       
@@ -45,19 +58,16 @@ export class Pathfinder {
       } else {
         visited.push(current);
       }
-
-      if (current[0] === goal[0] && current[1] === goal[1]){
-        break;
-      }
-
+      
       const neighbors = this.getNeighbors(current, isWalkable);
       for (const neighbor of neighbors){
-        const costFromStart = current[4] + this.getCost([current[0], current[1]], neighbor);
-        const costToGoal = this.getCost(neighbor, goal);
+        const costFromStart = current[4] + costFN([current[0], current[1]], neighbor);
+        const costToGoal = costFN(neighbor, goal);
         frontier.push(this.makeNode(neighbor, [current[0], current[1]], costFromStart, costToGoal));
       }
     }
 
+    // Reconstruct path
     const path: Path = [];
     let current = visited.pop();
     while (current){
@@ -71,10 +81,12 @@ export class Pathfinder {
       current = visited.find((v) => v[0] === current[2] && v[1] === current[3]);
     }
 
+    frontier.clear();
+    visited.length = 0;
     return path;
   }
 
-  private getNeighbors = (node: FrontierNode, isWalkable: IsWalkable): Cell[] => {
+  private getNeighbors = (node: FrontierNode, isWalkable: IsWalkableFN): Cell[] => {
     const [x, y, previousX, previousY] = node;
 
     return [
@@ -100,9 +112,5 @@ export class Pathfinder {
       costFromStart,
       costToGoal
     ];
-  }
-
-  private getCost = (a: Cell, b: Cell): number => { 
-    return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
   }
 }
